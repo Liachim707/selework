daa3ac818fc930a1b14f88e10606dc4793bf245b
Liachim707 <mcudanov55@gmail.com>
2026-09-21 17:06:20 +0700

    Создан проект

--- src/test/java/homework/DialogTest.java
+++ src/test/java/homework/DialogTest.java
@@ -1,4 +1,51 @@
 package homework;
 
-public class DialogTest {
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+class DialogTest {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1280x800";
+    }
+
+    @Test
+    void shouldOpenAndCloseDialog() {
+        open("https://jqueryui.com/dialog/");
+
+        // Переходим в iframe с демонстрацией
+        switchTo().frame($("iframe.demo-frame"));
+
+        // Проверяем, что кнопка открытия существует
+        $("#opener").shouldBe(visible);
+
+        // Открываем модальное окно
+        $("#opener").click();
+
+        // Проверяем, что окно открылось
+        $(".ui-dialog")
+                .shouldBe(visible);
+
+        // Проверяем заголовок
+        $(".ui-dialog-title")
+                .shouldHave(text("Basic dialog"));
+
+        // Проверяем содержимое
+        $(".ui-dialog-content")
+                .shouldBe(visible)
+                .shouldHave(text("This is the default dialog"));
+
+        // Закрываем окно
+        $(".ui-dialog-titlebar-close").click();
+
+        // Проверяем, что окно закрылось
+        $(".ui-dialog")
+                .shouldNotBe(visible);
+    }
 }
--- src/test/java/homework/DroppableTest.java
+++ src/test/java/homework/DroppableTest.java
@@ -1,4 +1,66 @@
 package homework;
 
-public class DroppableTest {
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+class DroppableTest {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1280x800";
+    }
+
+    @Test
+    void shouldDragElementIntoDroppableArea() {
+        open("https://jqueryui.com/droppable/");
+
+        // Переключаемся в iframe
+        switchTo().frame($("iframe.demo-frame"));
+
+        // Проверяем начальное состояние
+        $("#draggable").shouldBe(visible);
+        $("#droppable")
+                .shouldHave(text("Drop here"))
+                .shouldNotHave(cssClass("ui-state-highlight"));
+
+        // Выполняем drag-and-drop через jQuery UI
+        executeJavaScript("""
+            const draggable = $('#draggable');
+            const droppable = $('#droppable');
+
+            const draggableOffset = draggable.offset();
+            const droppableOffset = droppable.offset();
+
+            draggable.trigger({
+                type: 'mousedown',
+                which: 1,
+                pageX: draggableOffset.left + 10,
+                pageY: draggableOffset.top + 10
+            });
+
+            draggable.trigger({
+                type: 'mousemove',
+                which: 1,
+                pageX: droppableOffset.left + 10,
+                pageY: droppableOffset.top + 10
+            });
+
+            draggable.trigger({
+                type: 'mouseup',
+                which: 1,
+                pageX: droppableOffset.left + 10,
+                pageY: droppableOffset.top + 10
+            });
+        """);
+
+        // Проверяем результат перемещения
+        $("#droppable")
+                .shouldHave(cssClass("ui-state-highlight"))
+                .shouldHave(text("Dropped!"));
+    }
 }
